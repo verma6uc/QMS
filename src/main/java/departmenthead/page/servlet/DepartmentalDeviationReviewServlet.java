a4dd1b1881ca6d65a26749d95bedc6ee7b03dabb
jainaditya18j <aditya.jain@leucinetech.com>
2024-05-05 21:55:16 +0530

    Department Head Commit Servlet and jsp page

---
+++ src/main/java/departmenthead/page/servlet/DepartmentalDeviationReviewServlet.java
@@ -0,0 +1,25 @@
+package departmenthead.page.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DepartmentalDeviationReviewServlet extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.getRequestDispatcher("/departmenthead/departmentaldeviationreview.jsp").forward(request, response);
+		
+		
+	}
+
+}
